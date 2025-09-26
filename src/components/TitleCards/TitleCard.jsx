21c23e7d22d5d3b3b06625c4cba324dd9d3002c3
abdelhalim <abdelhalim@gmail.com>
2025-09-26 07:04:18 +0300

    build and initialize the sign up and sign in page and fetch data from TMDB site and display it in my site instead of the dummy data now i will think to remove those summy file data in the future but first lets finish this site

--- src/components/TitleCards/TitleCard.jsx
+++ src/components/TitleCards/TitleCard.jsx
@@ -1,11 +1,25 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRef } from 'react';
 import './TitleCard.css';
-import card_data from '../../image/cards/Cards_data';
+// import card_data from '../../image/cards/Cards_data';
 
 const TitleCard = ({title , category}) => {
+  const [apiData , setApiData] = useState([]);
 
   const cardsRef = useRef();
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZTY4YTdlZWNlMGYwMWNlNWMyODBlNzIzNTg3ODQ1MCIsIm5iZiI6MTc1ODg1NTk2Ni4wMTgsInN1YiI6IjY4ZDYwMzFlOTc1YWMzYmMzY2M4MzQ0MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.rihS9N_bywmQ9_xP_9SGazhNZtWzpXIekFlHwGc9q1o'
+    }
+  };
+  
+  // fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
+  //   .then(res => res.json())
+  //   .then(res => setApiData(res.results))
+  //   .catch(err => console.error(err));
 
   const handleWheel = (event) => {
     event.preventDefault();
@@ -14,10 +28,13 @@ const TitleCard = ({title , category}) => {
 
   // you can remove the array dependencies on this useEffect
   useEffect(() => {
+    // Fetching the data from the TMDB movies
+    fetch(`https://api.themoviedb.org/3/movie/${category?category:"now_playing"}?language=en-US&page=1`, options)
+    .then(res => res.json())
+    .then(res => setApiData(res.results))
+    .catch(err => console.error(err));
+
     cardsRef.current.addEventListener('wheel' , handleWheel);
-    // return () => {
-    //   cardsRef.current.removeEventListener('wheel' , handleWheel);
-    // }
   },[cardsRef])
 
   return (
@@ -26,10 +43,10 @@ const TitleCard = ({title , category}) => {
       {/* also you can remove the onWheel event */}
       <div className="card-container" ref={cardsRef} onWheel={handleWheel}>
         {
-          card_data.map((card , index) => (
+          apiData.map((card , index) => (
             <div className="card" key={index}>
-              <img src={card.image} alt="" />
-              <p>{card.name}</p>
+              <img src={`https://image.tmdb.org/t/p/w500${card.backdrop_path}`} alt="" />
+              <p>{card.original_title}</p>
             </div>
           ))
         }
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
+import logo from '../../image/logo.png';
 
 const Login = () => {
+  const [signState , setSignState] = useState("Sign In")
+
   return (
-    <div className='login'>Login</div>
+    <div className='login'>
+      <img src={logo} className='login-logo' alt="" />
+      <div className='login-form'>
+        <h2>{signState}</h2>
+        
+        <form>
+          {signState == "Sign Up" ? <input type="text" placeholder='Your name' /> : <></>}
+          <input type="email" placeholder='Ahmed Ali' />
+          <input type="password" placeholder='Password' />
+
+          <button>{signState}</button>
+
+          <div className="form-help">
+            <div className="remember">
+              <input type="checkbox" />
+              <label htmlFor="">Remember Me</label>
+            </div>
+
+            <p>Need Help?</p>
+          </div>
+        </form>
+        
+        <div className="form-switch">
+          {signState != "Sign Up" ? <p>New to Netflix? <span onClick={(() => {setSignState("Sign Up")})}>Sign up Now</span></p> : <p>Already have an account? <span onClick={(() => {setSignState("Sign In")})}>Sign in Now</span></p>}
+        </div>
+      </div>
+    </div>
   )
 }
 
